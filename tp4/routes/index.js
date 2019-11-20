8f7a1c4bdce2f8b4686bf39680e494d42c562dcc
JadouECE <jade.laville@edu.ece.fr>
2019-11-20 11:20:32 +0100

    Test commencement

--- tp4/routes/index.js
+++ tp4/routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET films. */
+router.get('/movies', function(req, res, next) => {
+  const {id}=req.params;
+  const movie = _.find(movie,["id",id]);
+  res.render('index', { title: 'Express' });
+});
+
+/* GET films par id */
+router.get('/movies/:id', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* PUT film par nom */
+router.get('/movies/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* UPDATE film par id */
+router.get('/movies/:id', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* DELETE film par id */
+router.get('/movies/id', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 module.exports = router;
+
